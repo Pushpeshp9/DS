30714aa1c7ca93376ab78aaeaf9f450e3d27545d
Pushpeshp9 <56801211+Pushpeshp9@users.noreply.github.com>
2019-11-11 22:46:18 +0530

    Ohohoho

--- names.c
+++ names.c
@@ -1,43 +1,36 @@
- #include <stdio.h>
-    #include <string.h>
-    void main()
-    {
- 
-        char name[10][8], tname[10][8], temp[8];
-        int i, j, n;
- 
-        printf("Enter the value of n \n");
-        scanf("%d", &n);
-        printf("Enter %d names n \n", n);
- 
-        for (i = 0; i < n; i++) 
-        {
-            scanf("%s", name[i]);
-            strcpy(tname[i], name[i]);
-        }
- 
-        for (i = 0; i < n - 1 ; i++)
-        {
-            for (j = i + 1; j < n; j++)
-            {
-                if (strcmp(name[i], name[j]) > 0) 
-                {
-                    strcpy(temp, name[i]);
-                    strcpy(name[i], name[j]);
-                    strcpy(name[j], temp);
-                }
-            }
-        }
- 
-        printf("\n----------------------------------------\n");
-        printf("Input NamestSorted names\n");
-        printf("------------------------------------------\n");
- 
-        for (i = 0; i < n; i++) 
-        {
-            printf("%s\t\t%s\n", tname[i], name[i]);
-        }
- 
-        printf("------------------------------------------\n");
- 
-    }
+#include<stdio.h>
+int main(int argc,char *argv[])
+{
+ FILE *fs,*ft;
+ int ch;
+ if(argc!=3)
+ {
+  printf("Invalide numbers of arguments.");
+  return 1;
+ }
+ fs=fopen(argv[1],"r");
+ if(fs==NULL)
+ {
+  printf("Can't find the source file.");
+  return 1;
+ }
+ ft=fopen(argv[2],"w");
+ if(ft==NULL)
+ {
+  printf("Can't open target file.");
+  fclose(fs);
+  return 1;
+ }
+
+ while(1)
+ {
+  ch=fgetc(fs);
+  if (feof(fs)) break;
+  fputc(ch,ft);
+ }
+
+ fclose(fs);
+ fclose(ft);
+ return 0;
+}
+
